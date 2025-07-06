8ebdefab653d89f0a38db7c18b97948b5619bf1d
NR1000-coder <nrminseyfullayeva@gmail.com>
2025-07-06 14:49:32 +0000

    beginAndModels

---
+++ api/models.py
@@ -0,0 +1,152 @@
+class News:
+
+    def __init__(self, tit, des, img):
+        self.tit = tit
+        self.des = des
+        self.img = img
+
+news1 = News(
+    "The Verge's summer in and out list",
+    "In our second annual trend forecast, The Verge staff weighs in on Labubus, tariffs, The Hague, and AI slop.",
+    "https://picsum.photos/400?random=1"
+)
+
+news2 = News(
+    "Lenovo Chromebook Plus 14 review:",
+    "﻿An OLED display, a fast processor, 16GB of RAM, and all-day battery life for $749? That's a lot to like.",
+    "https://picsum.photos/400?random=2"
+)
+
+news3 = News(
+    "Whoop MG review: ",
+    "a big whoop for a small crowd. I regret to say it's still only athletes who should give a whoop about Whoop.",
+    "https://picsum.photos/400?random=3"
+)
+
+news4 = News(
+    "Cyberpunk",
+    "Netflix's Cyberpunk 2077 anime spinoff is coming back for a second 10-episode season with an entirely new story.",
+    "https://picsum.photos/400?random=4"
+)
+
+news5 = News(
+    "Futureproof",
+    "AI tools are flooding the culture ecosystem — and no corner of the arts space is immune. In this series, we're looking at the ways artists are embracing AI, pushing back on it, or trying their best to find an equilibrium with a new technology that's both sweeping and destabilizing. ",
+    "https://picsum.photos/400?random=5"
+)
+
+news6 = News(
+    "The best Bluetooth trackers for Apple and Android phones",
+    "For those of us who don't ever seem to know where our keys, wallets, and remote controls have gone.",
+    "https://picsum.photos/400?random=6"
+)
+
+news7 = News(
+    "New Galaxy Z Fold 7",
+    "The latest Fold in the flesh, ahead of its expected launch at Unpacked next week",
+    "https://picsum.photos/400?random=7"
+)
+
+news8 = News(
+    "The best robot vacuum and mop",
+    "Writer's note: Amazon Prime Day kicks off on July 8th; however, if you want to get a head start on your online shopping, we've rounded up the best early Prime Day deals you can already get.",
+    "https://picsum.photos/400?random=8"
+)
+
+news9 = News(
+    "Here are the letters",
+    "Trump's Justice Department promised to release tech giants from legal liability to stop them from banning TikTok.",
+    "https://picsum.photos/400?random=9"
+)
+
+news10 = News(
+    "Apple's colorful Watch",
+    "We've also found deals on Amazon's Echo Frames, a 512GB microSD card under $30, and an affordable OLED TV.",
+    "https://picsum.photos/400?random=10"
+)
+
+news11 = News(
+    "The best phone to buy right now",
+    "Flagship phones cost a small fortune these days. We can help you pick the right one and get the most for your money.",
+    "https://picsum.photos/400?random=11"
+)
+
+news12 = News(
+    "The best deals on MacBooks right now",
+    "Apple's latest MacBook Pro models are already falling in price, as is the new M4 Mac Mini.",
+    "https://picsum.photos/400?random=12"
+)
+
+news13 = News(
+    "Lego's latest buildable arcade machine",
+    "The set will launch on August 1st, 2025, for $39.99.",
+    "https://picsum.photos/400?random=13"
+)
+
+news14 = News(
+    "Samsung seems to have leaked its own trifold phone design",
+    "The upcoming rival to Huawei's dual-hinged foldable may be officially unveiled next week.",
+    "https://picsum.photos/400?random=14"
+)
+
+news15 = News(
+    "Google's customizable Gemini chatbots",
+    "Gems are now available directly in the side panel of Google Docs, Slides, Sheets, Drive, and Gmail, allowing users to access custom chatbots they've created or a selection of pre-made offerings without switching between apps.",
+    "https://picsum.photos/400?random=15"
+)
+
+news16 = News(
+    "Racist videos made with AI are going viral on TikTok",
+    "Videos that may have been created with Google Veo 3 depict racist tropes targeting Black people, among others, according to Media Matters.",
+    "https://picsum.photos/400?random=16"
+)
+
+news17 = News(
+    "The best budget robot vacuums",
+    "You don't have to spend a fortune to have a robot clean your floors. Here's our pick of the most cost-effective bots you can buy right now.",
+    "https://picsum.photos/400?random=17"
+)
+
+news18 = News(
+    "Phil Spencer isn't retiring",
+    "Spencer has headed up Microsoft's Xbox efforts since 2014.",
+    "https://picsum.photos/400?random=18"
+)
+
+news19 = News(
+    "Xbox hit hard with layoffs",
+    "On July 2nd, Microsoft announced sweeping layoffs affecting as many as 9,000 employees, and they had a big impact on the company's Xbox studios.",
+    "https://picsum.photos/400?random=19"
+)
+
+news20 = News(
+    "Google's fix for Pixel 6A battery",
+    "Affected phones will have their battery capacity reduced, but could be eligible for a free replacement.",
+    "https://picsum.photos/400?random=20"
+)
+
+news21 = News(
+    "Helldivers 2 is the first PlayStation",
+    "Sony is bringing its popular live-service third-person shooter to more players.",
+    "https://picsum.photos/400?random=21"
+)
+
+news22 = News(
+    "Adding calendar events",
+    "Finally, a useful AI feature on our phones.",
+    "https://picsum.photos/400?random=22"
+)
+
+news23 = News(
+    "How Nintendo locked down the Switch 2's USB-C port",
+    "We whipped out a USB-C PD analyzer to test how Nintendo walled its garden this time around.",
+    "https://picsum.photos/400?random=23"
+)
+
+news24 = News(
+    "Blizzard is winding down support",
+    "Going forward Warcraft Rumble will receive bug fixes and seasonal events but no new content.",
+    "https://picsum.photos/400?random=24"
+)
+
+news_list = [news1, news2, news3, news4, news5, news6, news7, news8, news9, news10, news11, news12, news13, news14, news15, news16, news17, news18, news19, news20, news21, news22, news23, news24]
